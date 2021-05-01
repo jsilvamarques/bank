a5113be988978b5bfc789fd21f048b1f9f9ea6d7
Jefferson Marques da Silva <jefferson_marques17@hotmail.com>
2021-05-01 19:53:38 -0300

    inclusao do chart helm

--- authorization-server/src/test/java/br/com/impacta/bank/authorizationserver/AuthorizationServerApplicationTests.java
+++ authorization-server/src/test/java/br/com/impacta/bank/authorizationserver/AuthorizationServerApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthorizationServerApplicationTests {
+public class AuthorizationServerApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
